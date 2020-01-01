37eef0afaec21f3c8c46b53beb12a8b2c8431976
pwang96 <96pwang@gmail.com>
2020-01-01 10:01:43 -0500

    new website first draft

--- index.js
+++ index.js
@@ -2,63 +2,13 @@ var currPage;
 
 $(function() {
 	bindNavItems();
-	animateContactIcons();
-	showSlides(0);
-	animateVerb();
 });
 
 function bindNavItems() {
-	$("ul.navbar-nav").children().click(function() {
-		$("#home, #about, #projects, #resume, #contact").addClass("hidden");
-		$("#".concat($(this).text().toLowerCase())).removeClass("hidden");
-		$("#hamburgernav").attr("aria-expanded", "false");
-		$("#hamburgernav").removeClass("in");
+	$("ul#navlist").children().click(function() {
+		$("#home, #about, #interests, #blog, #contact").addClass("hidden");
+		$("#".concat($(this).children("a").eq(0).attr("data-nav-section"))).removeClass("hidden");
+		$("ul#navlist").children().removeClass("active");
+		$(this).addClass("active");
 	});
-}
-
-function animateContactIcons() {
-	$(".contact-icon").hover(function() {
-		$(this).animate({height:180, width:180}, 200);
-	}, function() {
-		$(this).animate({height:150, width:150}, 200);
-	});
-}
-
-function bindHoverProjects() {
-	$(".project-row").hover(function() {
-		$(this).css({
-                transition : 'background-color 1s ease-in-out',
-                "background-color": "green"
-	})}, function() {
-		$(this).css({
-                transition : 'background-color 1s ease-in-out',
-                "background-color": "transparent"
-            })
-	});
-}
-
-function plusSlides(n) {
-	showSlides(slideIndex += n);
-}
-
-function showSlides(n) {
-	var i;
-	var slides = document.getElementsByClassName("measure-pic");
-	if (n > slides.length) {slideIndex = 1}
-	if (n < 1) {slideIndex = slides.length}
-	for (i=0; i<slides.length; i++) {
-		slides[i].style.display = "none";
-	}
-	slides[slideIndex-1].style.display = "block"
-;}
-
-function animateVerb() {
-	$(".changing-verb").typed({
-        strings: ["create.", "code.", "question.", "teach.", "explore.", "care."],
-        typeSpeed: 80,
-		backSpeed: 30,
-		backDelay: 1500, 
-		loop: true,
-		showCursor: true
-      });
 }
