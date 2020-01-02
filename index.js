905ca4d1a5dab4565101f855b7d5a9c31d89a370
pwang96 <96pwang@gmail.com>
2020-01-01 20:28:11 -0500

    bootstrap changes, added some blog posts

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-var currPage;
-
-$(function() {
-	bindNavItems();
-});
+var opened;
+function isMobileWidth() {
+    return $('#mobile-indicator').is(':visible');
+}
 
 function bindNavItems() {
 	$("ul#navlist").children().click(function() {
-		$("#home, #about, #interests, #blog, #contact").addClass("hidden");
-		$("#".concat($(this).children("a").eq(0).attr("data-nav-section"))).removeClass("hidden");
+		$("#home, #about, #blog, #contact").addClass("d-none");
+		$("#".concat($(this).children("a").eq(0).attr("data-nav-section"))).removeClass("d-none");
 		$("ul#navlist").children().removeClass("active");
 		$(this).addClass("active");
 	});
 }
+
+$(function() {
+	bindNavItems();
+
+	$('#sidebarCollapse').on('click', function () {
+		event.stopPropagation();
+		$('#sidebar, #content').toggleClass('active');
+	});
+});
+
+$("html").click(function() {
+	if (isMobileWidth() && $("#sidebar").hasClass("active")) {
+		$('#sidebar, #content').toggleClass('active');
+	}
+  });
